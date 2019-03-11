146a816bb45f85a7760aec599c8612b912df6c28
dspdamith <dspdamithse@gmail.com>
2019-03-11 19:57:59 +0530

    Dynamic Contents Update

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting;
+use App\Category;
+
+class FrontEndController extends Controller
+{
+    public function index(){
+        return view('index')->with('title', Setting::first()->site_name)
+                            ->with('categories', Category::take(5)->get());
+    }
+}
